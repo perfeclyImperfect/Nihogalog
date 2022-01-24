caeac3a28ab00aca646f496b3ef76a1a9a84bfb0
zalven88 <zalvendayao888@gmail.com>
2022-01-24 20:57:44 +0800

    adding sound example export

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,10 +2,28 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import FileResponse
 
 
 @csrf_exempt
 def text_converter(request):
+    data = json.loads(request.body.decode())
+    # response_sound = FileResponse(open(
+    #     'sound/example.mp3', 'rb'))
+    return JsonResponse(
+        {
+            'success': True,
+            'text_input': "Text input",
+            'language_selected': "Japanese",
+            'language_convert': "Tagalog",
+            'text_output': "Japanese",
+            'speech_output': "Response"
+        }
+    )
+
+
+@csrf_exempt
+def speech_converter(request):
     data = json.loads(request.body.decode())
     return JsonResponse(
         {
@@ -20,21 +38,8 @@ def text_converter(request):
 
 
 @csrf_exempt
-def speech_converter(request):
-    return JsonResponse(
-        {
-            'success': True,
-            'text_input': "Text input",
-            'language_selected': "Japanese",
-            'language_convert': "Tagalog",
-            'text_output': "Japanese",
-            'speech_output': "dir"
-        }
-    )
-
-
-@csrf_exempt
 def emotion_detection(request):
+    data = json.loads(request.body.decode())
     return JsonResponse(
         {
             'success': True,
@@ -49,6 +54,7 @@ def emotion_detection(request):
 
 @csrf_exempt
 def object_detection(request):
+    data = json.loads(request.body.decode())
     return JsonResponse(
         {
             'success': True,
@@ -63,6 +69,7 @@ def object_detection(request):
 
 @csrf_exempt
 def text_detection_translator(request):
+    data = json.loads(request.body.decode())
     return JsonResponse(
         {
             'success': True,
