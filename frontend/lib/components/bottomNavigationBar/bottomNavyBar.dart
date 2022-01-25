9ba2dd7e3e07440656a4c14738e9b4f758d37a97
Arvin Chu <arvinchu31@gmail.com>
2022-01-25 14:51:24 +0800

    added layout for home navigation bar added color and proper icons added new components bottomNavyBar.dart file added parts of bottomNavyBar added bottomNavyItem.dart and itemWidget.dart files parts of bottomNavyBar

---
+++ frontend/lib/components/bottomNavigationBar/bottomNavyBar.dart
@@ -0,0 +1,121 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+
+import 'parts/bottomNavyItem.dart';
+import 'parts/itemWidget.dart';
+import '../../../utility/hexColor.dart';
+
+class CustomAnimatedBottomBar extends StatelessWidget {
+  const CustomAnimatedBottomBar({
+    Key? key,
+    this.selectedIndex = 0,
+    this.showElevation = true,
+    this.iconSize = 24,
+    this.backgroundColor,
+    this.itemCornerRadius = 50,
+    this.containerHeight = 56,
+    this.animationDuration = const Duration(milliseconds: 270),
+    this.mainAxisAlignment = MainAxisAlignment.center,
+    required this.items,
+    required this.onItemSelected,
+    this.curve = Curves.linear,
+  })  : assert(items.length >= 2 && items.length <= 5),
+        super(key: key);
+
+  final int selectedIndex;
+  final double iconSize;
+  final Color? backgroundColor;
+  final bool showElevation;
+  final Duration animationDuration;
+  final List<BottomNavyBarItem> items;
+  final ValueChanged<int> onItemSelected;
+  final MainAxisAlignment mainAxisAlignment;
+  final double itemCornerRadius;
+  final double containerHeight;
+  final Curve curve;
+
+  @override
+  Widget build(BuildContext context) {
+    final bgColor = backgroundColor ?? Theme.of(context).bottomAppBarColor;
+
+    return Badge(
+      badgeColor: Theme.of(context).scaffoldBackgroundColor,
+      position: BadgePosition.topEnd(end: 30, top: -35),
+      toAnimate: false,
+      borderSide: BorderSide.none,
+      elevation: 1,
+      badgeContent: Container(
+        margin: const EdgeInsets.all(5),
+        child: MaterialButton(
+            shape: const CircleBorder(),
+            onPressed: items[selectedIndex].active ? () {} : null,
+            child: Ink(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(80.0)),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    HexColor('#F0B831'),
+                    HexColor('#962F4A'),
+                    HexColor('#1E307C'),
+                  ],
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: IconTheme(
+                  data: IconThemeData(
+                    size: iconSize,
+                    color: Colors.white,
+                  ),
+                  child: items[selectedIndex].icon,
+                ),
+              ),
+            )),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(60),
+            topLeft: Radius.circular(60),
+          ),
+          color: bgColor,
+          boxShadow: [
+            if (showElevation)
+              const BoxShadow(
+                color: Colors.black12,
+                blurRadius: 2,
+              ),
+          ],
+        ),
+        child: SafeArea(
+          child: Container(
+            width: double.infinity,
+            height: containerHeight,
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 8),
+            child: Row(
+              mainAxisAlignment: mainAxisAlignment,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: items.map((item) {
+                var index = items.indexOf(item);
+                return GestureDetector(
+                  onTap: () => onItemSelected(index),
+                  child: ItemWidget(
+                    item: item,
+                    iconSize: iconSize,
+                    isSelected: index == selectedIndex,
+                    backgroundColor: bgColor,
+                    itemCornerRadius: itemCornerRadius,
+                    animationDuration: animationDuration,
+                    curve: curve,
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/lib/components/bottomNavigationBar/parts/itemWidget.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import '../../../utility/hexColor.dart';
+import 'bottomNavyItem.dart';
+
+class ItemWidget extends StatelessWidget {
+  final double iconSize;
+  final bool isSelected;
+  final BottomNavyBarItem item;
+  final Color backgroundColor;
+  final double itemCornerRadius;
+  final Duration animationDuration;
+  final Curve curve;
+
+  const ItemWidget({
+    Key? key,
+    required this.item,
+    required this.isSelected,
+    required this.backgroundColor,
+    required this.animationDuration,
+    required this.itemCornerRadius,
+    required this.iconSize,
+    this.curve = Curves.linear,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      container: true,
+      selected: isSelected,
+      child: AnimatedContainer(
+        width: isSelected ? 100 : 50,
+        height: 40,
+        duration: animationDuration,
+        curve: curve,
+        decoration: BoxDecoration(
+          gradient: isSelected
+              ? LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    HexColor('#F0B831'),
+                    HexColor('#962F4A'),
+                    HexColor('#1E307C'),
+                  ],
+                )
+              : null,
+          borderRadius: BorderRadius.circular(itemCornerRadius),
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          physics: const NeverScrollableScrollPhysics(),
+          child: Container(
+            width: isSelected ? 100 : 50,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              // mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                IconTheme(
+                  data: IconThemeData(
+                    size: iconSize,
+                    color: isSelected
+                        ? item.activeColor.withOpacity(1)
+                        : item.inactiveColor ?? item.activeColor,
+                  ),
+                  child: item.icon,
+                ),
+                if (isSelected)
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 3),
+                    child: DefaultTextStyle.merge(
+                      style: TextStyle(
+                        color: item.activeColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      maxLines: 1,
+                      textAlign: item.textAlign,
+                      child: item.title,
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
