156589fe48e4f755414f33302775dbbff97a8502
zalven88 <zalvendayao888@gmail.com>
2022-02-22 18:11:12 +0800

    changing translator

--- backend/api/helper/translator.py
+++ backend/api/helper/translator.py
@@ -6,11 +6,18 @@ from django.http import FileResponse
 import os
 import speech_recognition as sr
 import nltk
+
 nltk.download('omw-1.4')
 
 
 import json
 import text2emotion as te
+
+
+import json
+from ibm_watson import ToneAnalyzerV3, SpeechToTextV1, LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
 
 class translator_helper:
 
@@ -22,12 +29,63 @@ class translator_helper:
         katsu = cutlet.Cutlet()
         return katsu.romaji(text_input)
 
+
+    def tone_analyzer_api(self):
+        authenticator = IAMAuthenticator('y3OpvtsXh5JF3gcKjYbpdY5FrQa30jqHCx8d5GUIXSSh')
+        tone_analyzer = ToneAnalyzerV3(
+            version='2017-09-21',
+            authenticator=authenticator
+        )
+        tone_analyzer.set_service_url('https://api.au-syd.tone-analyzer.watson.cloud.ibm.com/instances/9328f9f1-e7f3-4018-a761-5156649d74c7')
+        tone_analyzer.set_disable_ssl_verification(True)
+        return tone_analyzer
+
+    def speech_to_text_api(self):
+        authenticator = IAMAuthenticator('D5CP-CK2qnzcpU3XexRpZ75XYJ6mXunae-Y66oIeOqCU')
+        speech_to_text = SpeechToTextV1(
+            authenticator=authenticator
+        )
+        speech_to_text.set_service_url('https://api.au-syd.speech-to-text.watson.cloud.ibm.com/instances/6def0d7a-957c-4772-9d72-9db4fecc3785')
+        speech_to_text.set_disable_ssl_verification(True)
+        return speech_to_text 
+    
+    def text_to_text_api(self):
+        authenticator = IAMAuthenticator('PCksUVQHiSy6jSXXxCLtyAP1LT0wPi07P2VgCySOpCFT')
+        language_translator = LanguageTranslatorV3(
+            version='2018-05-01',
+            authenticator=authenticator
+        )
+
+        language_translator.set_service_url('https://api.kr-seo.language-translator.watson.cloud.ibm.com/instances/9f9e3b18-5c02-48d9-b07e-80c929c9c8e8')
+        language_translator.set_disable_ssl_verification(True)
+        return language_translator
+
+
     def text_to_emoition(self, text_input):
         translator = Translator()
         text_to_emotion = translator.translate( text_input, dest='en')
-        return te.get_emotion(text_to_emotion.text)
+        tone_analysis = self.tone_analyzer_api().tone( {'text': text_to_emotion.text}, content_type='application/json' ).get_result()
+        tone_recognize = {}
+        if tone_analysis and 'sentences_tone' in tone_analysis.keys():
+            for emotion in tone_analysis['sentences_tone']:
+                for tones in emotion['tones']:
+                    if tones['tone_id'] not in tone_recognize.keys():  tone_recognize[tones['tone_id']] = tones['score']
+                    else: tone_recognize[tones['tone_id']] += tones['score']
+            for tones in tone_analysis['document_tone']['tones']:
+                if tones['tone_id'] not in tone_recognize.keys():  tone_recognize[tones['tone_id']] = tones['score']
+                else: tone_recognize[tones['tone_id']] += tones['score']
+        return tone_recognize
+
 
     def text_to_text(self, text_input):
+        # translator_api = self.text_to_text_api()
+        # translation = translator_api.translate(
+        #     text='Hello, how are you today?',
+        #     # model_id= "en-fr",
+        #     source="en",
+        #     target="tl").get_result()
+        # print(json.dumps(translation, indent=2, ensure_ascii=False))
+
         translator = Translator()
         translate_text = translator.translate( text_input, dest=self.language_convert)
         return translate_text.text
